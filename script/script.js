399bc0a09574e68c0768b2e36c13e3683a3ad5bd
Maria Luiza M <maria.lmm.975@gmail.com>
2022-02-03 09:52:51 -0300

    Adiciona função para distribuir cartas

--- script/script.js
+++ script/script.js
@@ -1,32 +1,54 @@
-function chooseCards(){
-    let cards = parseInt(prompt('Com quantas cartas você deseja jogar (insira um valor par entre 4 e 14)?'));
-    while (cards % 2 !== 0 || cards < 4 || cards > 14){
-        cards = prompt('Valor inválido. Com quantas cartas você deseja jogar (insira um valor par entre 4 e 14)?');
-    }
-    return cards;
+let parrots = ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot', 'tripletsparrot', 'unicornparrot'];
+
+parrots.sort(comparador);
+
+function comparador() { 
+	return Math.random() - 0.5; 
 }
 
-function assembleGame(cards){
+function chooseGame(){
+    let game = parseInt(prompt('Com quantas cartas você deseja jogar (insira um valor par entre 4 e 14)?'));
+    while (game % 2 !== 0 || game < 4 || game > 14){
+        game = prompt('Valor inválido. Com quantas cartas você deseja jogar (insira um valor par entre 4 e 14)?');
+    }
+    return game;
+}
+
+function assembleGame(numberOfCards){
+    let cards = [];
+    for (let i = 0; i < numberOfCards / 2; i++){
+        cards[2 * i] = parrots[i];
+        cards[2 * i + 1] = parrots[i];
+    }
+
+    cards.sort(comparador);
     let game = document.querySelector('ul');
-    for (let i = 1; i <= cards; i += 1 ){
+    for (let i = 0; i < numberOfCards; i++){
         game.innerHTML = game.innerHTML + `
         <li onclick = "turnCard(this)" class="card">
             <div class = "back">
                 <img src="midias/front.png" alt="Papagaio">
             </div>
             <div class = "front">
-                <img src="midias/metalparrot.gif" alt="Papagaio Metal">
+                <img src="midias/${cards[i]}.gif" alt="Papagaio Metal">
             </div>
         </li>
         `
-        // const card = document.querySelector('li:last-child');
-
     }
 }
 
 function turnCard(card){
-    card.classList.toggle('selecionada');
+    card.classList.add('selecionada');
+    turned = document.querySelectorAll('.selecionada');
+
+    if (turned.length > 1){
+        setTimeout(function(){
+            for (let i = 0; i < turned.length; i++){
+                turned[i].classList.remove('selecionada')
+            }
+        }, 1000);    
+    }
 }
 
-const cards = chooseCards();
-assembleGame(cards);
+const numberOfCards = chooseGame();
+assembleGame(numberOfCards);
